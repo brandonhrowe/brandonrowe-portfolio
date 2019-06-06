0906cbb099bf13ef05cf209f8a18d8a8ca333a40
Brandon Rowe <brandonhrowe@gmail.com>
2019-06-06 08:07:24 -0400

    gear transforms working, right gears implemented, still needs finessing and is browser intensive

--- public/index.js
+++ public/index.js
@@ -13,9 +13,9 @@ const counterClockwiseGears = document.querySelectorAll(".counterclockwise");
 
 document.addEventListener("scroll", () => {
   clockwiseGears.forEach(gear => {
-    gear.setAttribute("transform", `rotate(${window.scrollY}deg)`);
+    gear.setAttribute("style", `transform: rotate(${window.scrollY / 5}deg)`);
   });
   counterClockwiseGears.forEach(gear => {
-    gear.setAttribute("transform", `rotate(-${window.scrollY}deg)`);
+    gear.setAttribute("style", `transform: rotate(-${window.scrollY / 5}deg)`);
   });
 });
