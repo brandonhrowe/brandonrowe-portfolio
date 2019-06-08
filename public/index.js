89c9209732b5aa699d34e2d5650477ec71413494
Brandon Rowe <brandonhrowe@gmail.com>
2019-06-07 21:35:45 -0400

    button to toggle gears

--- public/index.js
+++ public/index.js
@@ -10,12 +10,34 @@ const height = Math.max(
 );
 const clockwiseGears = document.querySelectorAll(".clockwise");
 const counterClockwiseGears = document.querySelectorAll(".counterclockwise");
+const gearButton = document.querySelector("#gear-toggle");
+const allGears = document.querySelectorAll(".gears");
 
 document.addEventListener("scroll", () => {
-  clockwiseGears.forEach(gear => {
-    gear.setAttribute("style", `transform: rotate(${window.scrollY / 5}deg)`);
-  });
-  counterClockwiseGears.forEach(gear => {
-    gear.setAttribute("style", `transform: rotate(-${window.scrollY / 5}deg)`);
-  });
+  if (window.getComputedStyle(allGears[0]).display === 'block') {
+    clockwiseGears.forEach(gear => {
+      gear.setAttribute("style", `transform: rotate(${window.scrollY / 5}deg)`);
+    });
+    counterClockwiseGears.forEach(gear => {
+      gear.setAttribute(
+        "style",
+        `transform: rotate(-${window.scrollY / 5}deg)`
+      );
+    });
+  }
+});
+
+gearButton.addEventListener("click", () => {
+  if (window.getComputedStyle(allGears[0]).display === 'block') {
+    allGears.forEach(gears => {
+      gears.setAttribute('style', 'display: none')
+    })
+    gearButton.innerHTML = "Want To See The Gears Running This Site? <strong>Click</strong> Here!"
+  } else {
+    allGears.forEach(gears => {
+      gears.setAttribute('style', 'display: block')
+    })
+    gearButton.innerHTML = "Having Issues With This Page? <strong>Click</strong> to Turn Gears Off"
+
+  }
 });
