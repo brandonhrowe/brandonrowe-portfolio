1c9d430cc7d8030445d812899a1f644aa31acbcb
Brandon Rowe <brandonhrowe@gmail.com>
2019-06-12 20:35:56 -0400

    experience list and dropdowns in progress

--- public/index.js
+++ public/index.js
@@ -12,11 +12,12 @@ const clockwiseGears = document.querySelectorAll(".clockwise");
 const counterClockwiseGears = document.querySelectorAll(".counterclockwise");
 const gearButton = document.querySelector("#gear-toggle");
 const allGears = document.querySelectorAll(".gears");
-const burger = document.querySelector("#burger")
-const dropdown = document.querySelector("#navbarDropdown")
+const burger = document.querySelector("#burger");
+const navDropdown = document.querySelector("#navbarDropdown");
+const dropdowns = document.querySelectorAll(".dropdown");
 
 document.addEventListener("scroll", () => {
-  if (window.getComputedStyle(allGears[0]).display === 'block') {
+  if (window.getComputedStyle(allGears[0]).display === "block") {
     clockwiseGears.forEach(gear => {
       gear.setAttribute("style", `transform: rotate(${window.scrollY / 5}deg)`);
     });
@@ -30,33 +31,44 @@ document.addEventListener("scroll", () => {
 });
 
 gearButton.addEventListener("click", () => {
-  if (window.getComputedStyle(allGears[0]).display === 'block') {
+  if (window.getComputedStyle(allGears[0]).display === "block") {
     allGears.forEach(gears => {
-      gears.setAttribute('style', 'display: none')
-    })
-    gearButton.innerHTML = "<strong>Want To See The Gears Running This Site? Click Here!</strong>"
+      gears.setAttribute("style", "display: none");
+    });
+    gearButton.innerHTML =
+      "<strong>Want To See The Gears Running This Site? Click Here!</strong>";
   } else {
     allGears.forEach(gears => {
-      gears.setAttribute('style', 'display: block')
-    })
-    gearButton.innerHTML = "<strong>Having Issues With This Page? Click to Turn Gears Off</strong>"
-
+      gears.setAttribute("style", "display: block");
+    });
+    gearButton.innerHTML =
+      "<strong>Having Issues With This Page? Click to Turn Gears Off</strong>";
   }
 });
 
-burger.addEventListener('click', ()=>{
-  if (burger.classList.contains('is-active')){
-    burger.classList.remove('is-active')
-    dropdown.classList.remove('is-active')
+burger.addEventListener("click", () => {
+  if (burger.classList.contains("is-active")) {
+    burger.classList.remove("is-active");
+    navDropdown.classList.remove("is-active");
   } else {
-    burger.classList.add('is-active')
-    dropdown.classList.add('is-active')
+    burger.classList.add("is-active");
+    navDropdown.classList.add("is-active");
   }
-})
+});
 
-dropdown.addEventListener('click', ()=>{
-  if (dropdown.classList.contains('is-active')){
-    burger.classList.remove('is-active')
-    dropdown.classList.remove('is-active')
+navDropdown.addEventListener("click", () => {
+  if (navDropdown.classList.contains("is-active")) {
+    burger.classList.remove("is-active");
+    navDropdown.classList.remove("is-active");
   }
-})
+});
+
+dropdowns.forEach(cell => {
+  cell.addEventListener("click", () => {
+    if (cell.classList.contains("is-active")) {
+      cell.classList.remove("is-active");
+    } else {
+      cell.classList.add("is-active");
+    }
+  });
+});
