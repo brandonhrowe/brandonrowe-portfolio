08cd133730b87ea7ea2c9cd6f9f1fa29e15edad4
Brandon Rowe <brandonhrowe@gmail.com>
2019-09-30 09:59:36 -0400

    header updated with name and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,54 @@
 import React from 'react'
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <div id="footer">
-                <div className="inner">
-                    <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
-                    </ul>
-                    <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="footer">
+        <div className="inner">
+          <ul className="icons">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/brandonhrowe/"
+                className="icon fa-linkedin"
+              >
+                <span className="label">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/brandonhrowe"
+                className="icon fa-github"
+              >
+                <span className="label">Github</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://medium.com/@brandon.h.rowe"
+                className="icon fa-medium"
+              >
+                <span className="label">Medium</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:brandonhrowe@gmail.com?subject=Greetings!"
+                className="icon fa-envelope-o"
+              >
+                <span className="label">Email</span>
+              </a>
+            </li>
+          </ul>
+          <ul className="copyright">
+            <li>&copy; Brandon Rowe</li>
+            <li>
+              Starter Design: <a href="http://html5up.net">HTML5 UP</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Footer
